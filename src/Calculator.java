8af1c81fcbf214adf8e1daa24abaa84fa0061e80
SUMEYYE TUNA <43057909+sumeyyetuna@users.noreply.github.com>
2019-01-19 11:30:58 -0800

    add changes

--- src/Calculator.java
+++ src/Calculator.java
@@ -6,7 +6,7 @@ public class Calculator {
         return num1-num2;
     }
     
-    public static void double multiply(double num1,double  num2){
+    public static double multiply(double num1,double  num2){
          return num1*num2;
     }
 }
