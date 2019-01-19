53aa6c1d0a0ac728a869a6c7714475ad6ed5330a
SUMEYYE TUNA <43057909+sumeyyetuna@users.noreply.github.com>
2019-01-19 13:55:00 -0800

    add divide method

--- src/Calculator.java
+++ src/Calculator.java
@@ -9,4 +9,7 @@ public class Calculator {
     public static double multiply(double num1,double  num2){
          return num1*num2;
     }
+    public static double divide(double num1,double  num2){
+        return num1/num2;
+    }
 }
